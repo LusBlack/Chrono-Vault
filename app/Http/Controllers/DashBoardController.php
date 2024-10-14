16f61e916dd0ad619c18fae6ca71307bdc61f949
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-10-14 23:07:36 +0100

    added middleware to check account activation

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Middleware\RedirectIfUserHasNotEnabledStripe;
+use App\Http\Middleware\RedirectIfUserHasNotEnabledPaystack;
 use Illuminate\Http\Request;
 
 class DashBoardController extends Controller
 {
 
     public function __construct() {
-        $this->middleware (['auth', 'verified', RedirectIfUserHasNotEnabledStripe::class]);
+        $this->middleware (['auth', 'verified', RedirectIfUserHasNotEnabledPaystack::class]);
     }
 
     public function __invoke() {
